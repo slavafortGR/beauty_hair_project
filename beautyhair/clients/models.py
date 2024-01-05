9be8cdfff893152d8b4b5c72c6efb7608dffd4a6
Slava Kalmykov <slavafort35@gmail.com>
2024-01-05 15:59:29 +0200

    created clients, contacts models

--- beautyhair/clients/models.py
+++ beautyhair/clients/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+
 class Client(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     gender = models.CharField(max_length=1)
     registration_date = models.DateTimeField(auto_now_add=True)
+
+
+class Contact(models.Model):
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
+    value = models.CharField(max_length=160)
+    value_type = models.IntegerField()
+    primary_contact = models.SmallIntegerField()
