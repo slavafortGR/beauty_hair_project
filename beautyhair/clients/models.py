687fed88813caf7badd95456b80cfbdda3755edc
Andrey Gusev <andrey@anix.name>
2024-01-08 00:37:41 +0200

    clarified Client and Contact moels

--- beautyhair/clients/models.py
+++ beautyhair/clients/models.py
@@ -2,16 +2,28 @@ from django.db import models
 
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45)
+    first_name = models.CharField(max_length=45, null=False, blank=False)
+    last_name = models.CharField(max_length=45, null=False, blank=False)
     gender = models.CharField(max_length=1)
     registration_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = 'clients'
+
 
 class Contact(models.Model):
+    CONTACT_KINDS = {
+        0: 'phone',
+        1: 'telegram',
+        2: 'viber',
+        3: 'whatsapp',
+        4: 'instagram',
+        5: 'facebook',
+    }
     owner = models.ForeignKey(Client, on_delete=models.CASCADE)
-    value = models.CharField(max_length=160)
-    kind = models.IntegerField(
-        help_text="types: 0 - phone, 1 - telegram, 2 - viber, 3 - whatsapp, 4 - instagram, 5 - facebook"
-    )
-    primary = models.BooleanField(default=False)
+    value = models.CharField(max_length=160, null=False, blank=False)
+    kind = models.IntegerField(null=False, blank=False)
+    primary = models.BooleanField(default=False, null=False)
+
+    class Meta:
+        db_table = 'contacts'
