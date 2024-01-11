6d7db94f19ee535809333f46c8014f86759adf33
Slava Kalmykov <slavafort35@gmail.com>
2024-01-11 23:50:25 +0200

    edited 'dashboard.html' file

--- beautyhair/clients/forms.py
+++ beautyhair/clients/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from models import *
+
 
 class AddClient(forms.Form):
     first_name = forms.CharField(max_length=45)
     last_name = forms.CharField(max_length=45)
     gender = forms.CharField(max_length=1)
-    contact = forms.CharField(max_length=160)
-    primary = forms.BooleanField()
+    # contact = forms.CharField(max_length=160)
+    # primary = forms.BooleanField()
--- beautyhair/clients/urls.py
+++ beautyhair/clients/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import get_clients
 
 urlpatterns = [
-    path('', get_clients)
+    path('', get_clients, name='clients')
 ]
