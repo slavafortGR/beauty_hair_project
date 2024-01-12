c4b55f11da226b3d38c4d74865ea536753240a4b
Slava Kalmykov <slavafort35@gmail.com>
2024-01-12 23:55:48 +0200

    created get_clients.html

--- beautyhair/clients/urls.py
+++ beautyhair/clients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import get_clients
 
+
 urlpatterns = [
-    path('', get_clients, name='clients')
+    path('', get_clients, name='get_clients')
 ]
--- beautyhair/clients/views.py
+++ beautyhair/clients/views.py
@@ -1,6 +1,18 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from clients.forms import AddClient
+from clients.models import Client
 
 
 def get_clients(request):
-    return HttpResponse("<h1>Clients</h1>")
+    if request.method == 'POST':
+        form = AddClient(request.POST)
+        if form.is_valid():
+            try:
+                Client.objects.create(**form.cleaned_data)
+                return redirect('get_clients')
+            except:
+                form.add_error('', 'Error')
+    else:
+        form = AddClient()
+    return render(request, 'clients/get_clients.html', {'form': form, 'title': 'Создать клиента'})
