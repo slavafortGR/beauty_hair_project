77715db4257c40139f104884d82746a90b642d7e
Slava Kalmykov <slavafort35@gmail.com>
2024-03-01 20:46:16 +0200

    editet urls.py file

--- beautyhair/clients/urls.py
+++ beautyhair/clients/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', clients, name='clients'),
     path('<int:pk>', view_client, name='view_client'),
     path('new/', new_client, name='new_client'),
-    path('edit/<int:pk>/', edit_client, name='edit_client'),
-    path('add_contact/<int:pk>/', add_contact, name='add_contact'),
-    path('delete/<int:pk>', delete_client, name='delete_client'),
+    path('<int:pk>/edit/', edit_client, name='edit_client'),
+    path('<int:pk>/add_contact/', add_contact, name='add_contact'),
+    path('<int:pk>/delete/', delete_client, name='delete_client'),
 ]
